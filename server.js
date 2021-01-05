ca30bb7c6942814a974c300f7f90a10917431547
nixls <28026071+nixls@users.noreply.github.com>
2021-01-05 15:00:48 +0100

    Add CSS

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const knex = require('knex');
 const bodyParser = require("body-parser");
 
+// Express instance
 const app = express();
 
+// Database connection
 const db = knex({
     client: 'pg',
     connection: process.env.DATABASE_URL
 });
 
+// Create table if it doesn't exist
 db.schema.hasTable('task').then(hasTable => {
     if (!hasTable) {
         console.log("database doesn't have the task table, creating");
@@ -26,13 +29,16 @@ db.schema.hasTable('task').then(hasTable => {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
 
+// GET /
 app.get("/", (req, res) => {
     db.select("*").from("task").then(data => {
         res.render('index', {todos: data});
     }).catch(err => res.status(400).json(err));
 });
 
+// POST /addTask
 app.post("/addTask", (req, res) => {
     const {textTodo} = req.body;
     db("task").insert({task: textTodo}).returning("*").then(_=> {
@@ -42,6 +48,7 @@ app.post("/addTask", (req, res) => {
     });
 });
 
+// PUT /moveTaskDone
 app.put("/moveTaskDone", (req, res) =>{
     const { name, id } = req.body;
     if (name === "todo") {
